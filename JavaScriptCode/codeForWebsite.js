c2a33ab3a1e37ad467c041a8a0d75f4c90b8b5ba
noamgreenstein <106999318+noamgreenstein@users.noreply.github.com>
2022-12-20 14:04:07 -0500

    hello

---
+++ JavaScriptCode/codeForWebsite.js
@@ -0,0 +1,16 @@
+// const ng = document.querySelector("p1");
+// ng.innerHTML = ng.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+// anime.timeline()
+//     .add({
+//              targets: 'p1 .letter',
+//              opacity: [0,1],
+//              easing: "easeInOutQuad",
+//              duration: 2250,
+//              delay: (el, i) => 150 * (i+1)
+//          })
+
+
+
+
+
+
